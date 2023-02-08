60b509fc29d5989c3a014c732027f20f9396dc99
dunCan <f0uad.f@hotmail.com>
2023-02-08 09:46:06 +0300

    added dark-mode support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), react()]
 });
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const SITE_URL = '/'
+export const SITE_NAME = 'S.B. Géoservices Inc.'
+export const SITE_AUTHOR = 'Sofiane Boulila'
+export const SITE_DESCRIPTION = 'Entreprise de Géoservices'
