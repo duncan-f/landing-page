aa23e702b2d37960061d51397259162b2b1ff320
dunCan <f0uad.f@hotmail.com>
2023-02-09 10:03:49 +0300

    added dropdown menu

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -3,16 +3,12 @@ import React, { Fragment } from 'react'
 import { IoMenu } from 'react-icons/io5/index.js'
 import DropdownMenuItem from './DropdownMenuItem'
 
-interface Props {
-  tags: string[]
-}
-
-export default function DropdownMenu({ tags }: Props) {
+export default function DropdownMenu() {
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="md:hidden relative inline-block text-left">
       <div>
         <Menu.Button
-          className="inline-flex justify-center rounded-md border border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
+          className="inline-flex justify-center rounded-md border border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-teal-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
           aria-label="menu"
         >
           <IoMenu className="h-5 w-5" />
@@ -28,21 +24,20 @@ export default function DropdownMenu({ tags }: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-orange-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-teal-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-zinc-400 dark:divide-zinc-700">
           <div className="py-1">
-            <div className="px-3 py-2 uppercase font-bold text-xs">
-              Categories
-            </div>
-            {tags.map(tag => {
-              return (
-                <DropdownMenuItem
-                  key={tag}
-                  href={`/categories/${tag.toLowerCase()}`}
-                >
-                  {tag}
-                </DropdownMenuItem>
-              )
-            })}
+            <DropdownMenuItem href="/">
+              A propos de nous
+            </DropdownMenuItem>
+            <DropdownMenuItem href="/services">
+              Nos Services
+            </DropdownMenuItem>
+            <DropdownMenuItem href="/projects">
+              Nos Projets
+            </DropdownMenuItem>
+            <DropdownMenuItem href="/contact">
+              Contact
+            </DropdownMenuItem>
           </div>
         </Menu.Items>
       </Transition>
