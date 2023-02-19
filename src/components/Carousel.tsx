fac0daa6dcfd08997d42ef33d9e8146535856548
dunCan <f0uad.f@hotmail.com>
2023-02-20 01:02:15 +0300

    issue with tsx component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Swipe from "react-easy-swipe";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 interface Props {
   services: any[];
@@ -18,11 +20,69 @@ class Carousel extends React.Component<Props, State> {
     };
   }
 
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.paused === false) {
+        let newSlide =
+          this.state.currentSlide === this.props.services.length - 1
+            ? 0
+            : this.state.currentSlide + 1;
+        this.setState({ currentSlide: newSlide });
+      }
+    }, 5000);
+  }
+
+  nextSlide = () => {
+    let newSlide =
+      this.state.currentSlide === this.props.services.length - 1
+        ? 0
+        : this.state.currentSlide + 1;
+    this.setState({ currentSlide: newSlide });
+  };
+
+  prevSlide = () => {
+    let newSlide =
+      this.state.currentSlide === 0
+        ? this.props.services.length - 1
+        : this.state.currentSlide - 1;
+    this.setState({ currentSlide: newSlide });
+  };
+
+  setCurrentSlide = (index: number) => {
+    this.setState({ currentSlide: index });
+  };
+
   render() {
     return (
       <div className="left-0 top-0">
-        <div className="flex w-full h-screen relative justify-center items-center">
-        This is a test!!!
+        <div className="w-full h-screen relative">
+          <AiOutlineLeft
+            onClick={this.prevSlide}
+            className="absolute left-10 text-3xl inset-y-1/2 text-white cursor-pointer"
+          />
+
+          <div className="absolute w-full flex justify-center bottom-6">
+            {this.props.services.map((element, index) => {
+              return (
+                <div
+                  className={
+                    index === this.state.currentSlide
+                      ? "h-2 w-2 bg-blue-700 rounded-full mx-2 mb-2 cursor-pointer"
+                      : "h-2 w-2 bg-white rounded-full mx-2 mb-2 cursor-pointer"
+                  }
+                  key={index}
+                  onClick={() => {
+                    this.setCurrentSlide(index);
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+
+          <AiOutlineRight
+            onClick={this.nextSlide}
+            className="absolute right-10 text-3xl inset-y-1/2 text-white cursor-pointer"
+          />
         </div>
       </div>
     );
